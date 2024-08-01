91b2d78f743e6eace7e743ab2f927f6b1c8b4f29
Vatsal Mistry <mistryvatsal11@gmail.com>
2024-08-02 00:55:09 +0530

    Modified content

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <a href="mailto:vatsal@solvdd.com">Contact</a>
       </footer>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Solvdd Technology Solutions</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Welcome to Solvdd Technology Solutions" />
+        <p className="description">Solving tomorrow, today!</p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
